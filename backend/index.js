214187669a293b62dc1fb9a4ab27b0e37b5574c6
Jijo Joseph <jijojoseph@Jijos-MacBook-Air.local>
2024-07-31 18:46:08 +0530

    cors change

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,13 @@ var corsOptions = {
   };
   app.use(cors(corsOptions));
 
+  app.options('*', cors())
+
 if (!process.env.DATABASE_URL) {
     console.error('DATABASE_URL environment variable is not set');
     process.exit(1);
   }
-  
+
 const db = require('./configs/database');
 db.authenticate()
   .then(() => console.log('Database connected...'))
